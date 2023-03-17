e9b6f3b9a03504c56d96a73afbb0874952b43643
Tendai  Bandawa <tonderaibandawa@gmail.com>
2023-03-17 14:24:46 +0200

    Initial Commit

---
+++ Shopping Cart/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Shopping Cart
+//
+//  Created by Tendai Bandawa on 2023/03/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shopping Cart/Shopping_CartApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shopping_CartApp.swift
+//  Shopping Cart
+//
+//  Created by Tendai Bandawa on 2023/03/17.
+//
+
+import SwiftUI
+
+@main
+struct Shopping_CartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
